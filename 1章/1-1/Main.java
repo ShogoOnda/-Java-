5b12833bafe508c35fc6c80a3ae5d7a451b48fab
ShogoOnda <duo0116@gmail.com>
2022-07-17 14:08:19 +0000

    [Add] 1章 1-1

---
+++ 1章/1-1/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+  public static void main(String[] args){
+    System.out.println("RPG:スッキリ魔王征伐");
+    System.out.println("ver.0.1 by 湊");
+    System.out.println("<ただいま制医学集・製作中>");
+    System.out.println("プログラムを終了します");
+  }
+}
