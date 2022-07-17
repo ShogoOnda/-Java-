cde640eaa0c7e5cb8d07e45c9488f27ea234ec6a
ShogoOnda <duo0116@gmail.com>
2022-07-17 14:33:15 +0000

    [Add] 1-4

---
+++ 1章/1-4/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+  public static void main(String[] args){
+    double pi =3.14;
+    int pie = 5;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * pi);
+    System.out.println("パイの半径を倍にします");
+    // テキストの通り、pieではなく、piに代入しています。上書きしてはいけない失敗例です。
+    pi = 10;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * pi);
+  }
+}
