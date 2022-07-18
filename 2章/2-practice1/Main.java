97349fd8d537ab9c9abb80930ce987e70617f30c
ShogoOnda <duo0116@gmail.com>
2022-07-18 05:13:18 +0000

    [Add] 2-practice1 練習問題　　p95

---
+++ 2章/2-practice1/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+  public static void main(String[] args){
+    int x = 5;
+    int y = 10;
+    int m = x + y;
+    String ans = "x + y は" + m;
+    System.out.println(ans);
+  }
+}
