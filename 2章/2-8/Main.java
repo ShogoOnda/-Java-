bd12c6b26124cc473f916c82bad3f0a7250ed613
ShogoOnda <duo0116@gmail.com>
2022-07-18 03:50:01 +0000

    [Add] 2-8 異なる型同士の算術演算　　p84

---
+++ 2章/2-8/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main(String[] args){
+    double d = 8.5 / 2;
+    long l = 5 + 2L;
+    // 2(int型)を2.0(double型)に変換
+    System.out.println(d);
+    // 5(int型)を5L(long型)に変換
+    System.out.println(l);
+  }
+}
