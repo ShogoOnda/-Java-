821dc637c077b086a9b0900a8d70f117228dd95e
ShogoOnda <duo0116@gmail.com>
2022-07-22 08:37:05 +0000

    [Add] 3章 3-2 トイレの空きを待つ繰り返し処理（無限）　　p104

---
+++ 3章/3-2/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+  public static void main(String[] args){
+    boolean doorClose = true; //ここでtrueかfalseを代入
+    while (doorClose == true) {
+      System.out.println("ノックをする");
+      System.out.println("1分まつ");
+    }
+  }
+}
