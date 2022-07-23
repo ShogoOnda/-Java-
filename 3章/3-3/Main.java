b466eca41ab746243602fe5d8e76279cb1f2c8df
ShogoOnda <duo0116@gmail.com>
2022-07-23 14:10:55 +0000

    [Add] 3章 3-3 波カッコを省略した記述　　p107

---
+++ 3章/3-3/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main(String[] args){
+    boolean tenki = true; //ここでtrueかfalseを代入
+    if (tenki == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩に行きます");
+    }else　//ここで}の省略が可能
+      System.out.println("DVDを見ます");
+  }
+}
