cf2358a3080b4ad029a3dd3202ff2427f1968a27
ShogoOnda <duo0116@gmail.com>
2022-07-28 23:44:33 +0000

    [Add] 3章 3-6 あえてbreak文を書かない　　p123

--- 3章/3-5/Main.java
+++ 3章/3-5/Main.java
@@ -3,16 +3,16 @@ public class Main{
     System.out.println("あなたの運勢を占います");
     int fortune = 1;
     switch (fortune) {
-      case 1;  
+      case 1:
         System.out.println("大吉");
         //ここにbreakを入れ忘れている例
-      case 2;
+      case 2:
         System.out.println("中吉");
         break;
-      case 3;
+      case 3:
         System.out.println("吉");
         break;
-      default;
+      default:
         System.out.println("凶");
     }
   }
