e8ad820c5facf5b7a03e253aa997c114fff86097
ShogoOnda <duo0116@gmail.com>
2022-08-01 05:06:38 +0000

    [Fix] 4章 4-11 50点以上の科目の数を調べる　p159

--- 4章/4-10/Main.java
+++ 4章/4-10/Main.java
@@ -1,12 +1,12 @@
 public class Main{
   public static void main(String[] args){
     int[] scores = {20, 30, 40, 50, 80};
-    int count = 0;
+    int sum = 0;
     for (int i = 0; i < scores.length; i++) {
-      if (scores[i] >= 50) {
-        count++;
-      }
+      sum += scores[i];
     }
-    System.out.println("50点以上の科目の数は:" + count);
+    int avg = sum / scores.length;
+    System.out.println("合計点:" + sum);
+    System.out.println("平均点:" + avg);
   }
 }
