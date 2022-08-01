2381b8a5ba2f0b837cb120ccaf012772cbfd01ae
ShogoOnda <duo0116@gmail.com>
2022-08-01 13:52:17 +0000

    [Add] 5章 5-11 オーバーロード（引数の数が異なる場合） 　p207

---
+++ 5章/5-11/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+  // 一つ目のメソッド
+  public static int add(int x, int y){
+    return x + y;
+  }
+  // 二つ目のメソッド
+  public static int add(int x, int y, int z){
+    return x + y + z;
+  }
+  public static void main(String[] args){
+    System.out.println("10 + 20 = " + add(10, 20));
+  }
+}
