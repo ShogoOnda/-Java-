b9c88b22dac2e21a81b756405175c34a9c3f5140
ShogoOnda <duo0116@gmail.com>
2022-08-03 13:49:35 +0000

    [Add] 6章 6-2,6-3 CalcLogic.javaとmainメソッドを分ける　p22９

---
+++ 6章/6-2 , 6-3/CalcLogic.java
@@ -0,0 +1,9 @@
+public class CalcLogic{
+  //メソッドがクラス内に記載されておらず、エラーになる例
+  public static int tasu(int a, int b) {
+    return (a + b);
+  }
+  public static int hiku(int a, int b) {
+    return (a - b);
+  }
+}
