eeada2b3a596024e93dfcc4c023e380ac0cdc168
ShogoOnda <duo0116@gmail.com>
2022-08-04 14:52:58 +0000

    [Add] 6章 6-6,6-9 calcapp.logicsの全クラスをインポートする　p239

---
+++ 6章/6-6 , 6-9/Calc.java
@@ -0,0 +1,12 @@
+package calcapp.main;
+import calcapp.logics.*;
+
+public class Calc{
+  public static void main(String[] args){
+    int a = 10; int b = 2;
+    //import文を追記して省略したが、後の学習後にコンパイル必要あり
+    int total = CalcLogic.tasu(a, b);
+    int delta = calcapp.logics.CalcLogic.hiku(a, b);
+    System.out.println("足すと" + total + "、引くと" + delta);
+  }
+}
