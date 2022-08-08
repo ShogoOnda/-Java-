fb0b1779f4578aea0bb1c151f005526719f64371
ShogoOnda <duo0116@gmail.com>
2022-08-08 14:37:14 +0000

    [Add] 8章 8-2 HeroクラスをJavaのコードで表したもの　p309

---
+++ 8章/8-2/Hero.java
@@ -0,0 +1,9 @@
+public class Hero{
+  String name;
+  int hp;
+  public void attack() {...};
+  public void sleep() {...};
+  public void sit(ubt sec) {...};
+  public void slip() {...};
+  public void run() {...};
+}
