5593e0f11602c823bf8bc8d84da71fbf70c65c9f
ShogoOnda <duo0116@gmail.com>
2022-08-11 10:40:59 +0000

    [Add] 9章 9-15 別のコンストラクタを呼び出す p361

---
+++ 9章/9-15/Hero.java
@@ -0,0 +1,19 @@
+public class Hero{
+  String name;
+  int hp;
+  
+  public void attack() {
+    System.out.println(this.name + "は攻撃した！");
+    System.out.println("敵に5ポイントのダメージをあたえた！");
+  }
+  
+  public Hero(String name) { //コンストラクタ①
+    this.hp = 100; //hpフィールドを100で初期化
+    this.name = name; //引数の値でnameフィールドを
+  }
+  
+  public Hero() { //コンストラクタ②
+    this.hp = 100;
+    this.name = "ダミー";
+  }
+}
