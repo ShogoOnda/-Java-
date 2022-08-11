4317cd33ccf5691b64b2e50052a26172cb9d4caa
ShogoOnda <duo0116@gmail.com>
2022-08-11 09:56:40 +0000

    [Add] 9章 9-13 コンストラクタをオーバーロードしたクラスの利用 p359

---
+++ 9章/9-13/Hero.java
@@ -0,0 +1,19 @@
+public class Hero{
+  String name;
+  int hp;
+  
+  public void attack() {
+    System.out.println(this.name + "は攻撃した！");
+    System.out.println("敵に5ポイントのダメージをあたえた！");
+  }
+  
+  public Hero(String name) {
+    this.hp = 100; //hpフィールドを100で初期化
+    this.name = name; //引数の値でnameフィールドを
+  }
+  
+  public Hero() {
+    this.hp = 100;
+    this.name = "ダミー";
+  }
+}
