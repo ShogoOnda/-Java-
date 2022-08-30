a65f371047e28986ec371dc48896efda219c14c0
ShogoOnda <duo0116@gmail.com>
2022-08-30 13:50:02 +0000

    [Add] 14章 14-7 名前が同じなら同じ勇者とするequals()を定義 p538

---
+++ 14章/14-7/Hero.java
@@ -0,0 +1,30 @@
+public class Hero {
+  int hp;
+  String name;
+  public void setHero(String name) {
+    if (name == null) {
+      throw new IllegalArgumentException("名前がnullである。処理を中断。");
+    }
+    if (name.length() <= 1) {
+      throw new IllegalArgumentException("名前が短すぎる。処理を中断。");
+    }
+    if (name.length() >= 8) {
+      throw new IllegalArgumentException("名前が長すぎる。処理を中断。");
+    }
+    this.name = name;
+  }
+  public String toString() {
+    return "名前：" + this.name + "/HP：" + this.hp;
+  }
+  public boolean equals(Object o) {
+    if (this == o) {return true;}
+    if (o instanceof Hero) {
+      Hero h = (Hero)o;
+      if (this.name.equals(h.name)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+  
